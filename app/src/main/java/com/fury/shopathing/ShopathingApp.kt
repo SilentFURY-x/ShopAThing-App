24e5a233befd7cc26fbde28abf0747686e2317ae
Arjun Tyagi <tyagiarjun1809@gmail.com>
2025-12-29 20:09:33 +0530

    project/folder structure created, activated hilt

---
+++ app/src/main/java/com/fury/shopathing/ShopathingApp.kt
@@ -0,0 +1,10 @@
+package com.fury.shopathing
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class ShopathingApp : Application() {
+    // This class triggers Hilt's code generation.
+    // No other code is needed here for now.
+}
