7ed133c57b7d288f8924673c558f070c945cf05c
Arjun Tyagi <tyagiarjun1809@gmail.com>
2025-12-29 19:59:00 +0530

    added libraries and dependencies (hilt, coil, retrofit, nav, icons)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,9 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    id("com.google.dagger.hilt.android")
+    // (for code generation):
+    id("kotlin-kapt")
 }
 
 android {
@@ -56,4 +59,24 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+    // --- ARCHITECTURE & NAVIGATION ---
+    // Hilt (Dependency Injection) - The glue of our app
+    implementation("com.google.dagger:hilt-android:2.51.1")
+    kapt("com.google.dagger:hilt-android-compiler:2.51.1")
+
+    // Navigation for Compose
+    implementation("androidx.navigation:navigation-compose:2.7.7")
+    implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
+
+    // --- NETWORK (Internet) ---
+    // Retrofit (API calls)
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    // Gson Converter (To turn JSON into Kotlin objects)
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+
+    // --- UI HELPERS ---
+    // Coil (Image Loading made for Compose)
+    implementation("io.coil-kt:coil-compose:2.6.0")
+    // Extended Icons (Optional, but good for shopping cart icons)
+    implementation("androidx.compose.material:material-icons-extended:1.6.8")
 }
